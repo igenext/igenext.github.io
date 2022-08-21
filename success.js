4a83a70604f477dd2266abe718496ca05f8d0ada
Inbarasan <inbarasainba8@gmail.com>
2022-08-21 16:38:40 +0530

    TARP

--- success.js
+++ success.js
@@ -3,8 +3,24 @@ let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
 
+const constraints = {
+	video: {
+	  width: {
+		min: 1280,
+		ideal: 1920,
+		max: 2560,
+	  },
+	  height: {
+		min: 720,
+		ideal: 1080,
+		max: 1440,
+	  },
+	  facingMode: "environment"
+	},
+  }
+
 camera_button.addEventListener('click', async function() {
-   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+   	let stream = await navigator.mediaDevices.getUserMedia(constraints);
 	video.srcObject = stream;
 });
 
